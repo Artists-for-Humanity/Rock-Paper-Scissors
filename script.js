6759daba9eca21e6b4271e363b8c183df6e4f1b4
Izayah <izayahfuller3@gmail.com>
2024-10-10 16:19:05 -0400

    finished this

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const selectionButtons = document.querySelectorAll("[data-selection]");
 const finalColumn = document.querySelector("[data-final-column]");
 const yourScoreSpan = document.querySelector("[data-your-score]");
+const computerScoreSpan = document.querySelector("[data-computer-score]");
 
 // Define a const variable for the computer score span similar to the player score span above
 /*** ADD CODE HERE***/
@@ -12,14 +13,24 @@ const SELECTIONS = [
     beats: "scissors",
   },
   // Add additional SELECTIONS
-  /*** ADD CODE HERE***/
-  /*** ADD CODE HERE***/
+  {
+  name: "paper",
+  emoji: "✋",
+  beats: "rock",
+  },
+  {
+    name: "scissors",
+    emoji: "✌",
+    beats: "paper",
+  }
+
 ];
 
 selectionButtons.forEach((selectionButton) => {
   selectionButton.addEventListener("click", (e) => {
     const selectionName = selectionButton.dataset.selection;
     // Add Console log described in the README
+    console.log("rockselected");
     /*** ADD CODE HERE***/
 
     const selection = SELECTIONS.find(
@@ -39,16 +50,24 @@ function makeSelection(selection) {
   const yourWinner = isWinner(selection, computerSelection);
   // Add const variable for that returns the computer as the winner
   /*** ADD CODE HERE***/
+  const computerWinner = isWinner(computerSelection, selection);
 
   // Add a function call for addSelectionResult function that displays the computer's selection
   addSelectionResult(computerSelection, computerWinner);
+  console.log({computerWinner, yourWinner,});
+  addSelectionResult(selection, yourWinner);
+
   /*** ADD CODE HERE***/
+//   function addSelectionResult() {
+// console.log(computerSelection)
+//   }
   
 
 
 // Add an if condition that checks if the computer is the winner AND increments its score
   if (yourWinner) incrementScore(yourScoreSpan);
-  /*** ADD CODE HERE***/
+  if (computerWinner) incrementScore(computerScoreSpan); 
+console.log({yourScoreSpan, computerScoreSpan})
 }
 
 function incrementScore(scoreSpan) {
